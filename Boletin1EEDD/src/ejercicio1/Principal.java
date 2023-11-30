c73cbd91c9c4598f35451a2cdf3f485f0fb10007
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2023-11-30 13:40:04 +0100

    adda

--- Boletin1EEDD/src/ejercicio1/Principal.java
+++ Boletin1EEDD/src/ejercicio1/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 
 		int[][] a = new int[10][10];
 
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				System.out.printf("%2d ", a[i][j]);
 			}
